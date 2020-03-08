97f0779bcb38551a8c48751b538e9f0561bb07c9
Arkadiusz <arkadiusz.omyla@exaco.pl>
2020-03-08 18:20:39 +0100

    refactor to common crud interface

--- pet-clinic-data/src/main/java/pl/arek/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/pl/arek/petclinic/services/PetService.java
@@ -2,11 +2,5 @@ package pl.arek.petclinic.services;
 
 import pl.arek.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
-
+public interface PetService extends CrudService<Pet, Long> {
 }
