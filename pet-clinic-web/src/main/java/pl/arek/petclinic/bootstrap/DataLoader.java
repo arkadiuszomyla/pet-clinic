8f7425d2aa88fac3e02d391a80b5f9d7eaac371a
Arkadiusz <arkadiusz.omyla@exaco.pl>
2020-03-12 12:14:57 +0100

    added pets and contact to dataloader

--- pet-clinic-web/src/main/java/pl/arek/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pl/arek/petclinic/bootstrap/DataLoader.java
@@ -3,11 +3,14 @@ package pl.arek.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.arek.petclinic.model.Owner;
+import pl.arek.petclinic.model.Pet;
 import pl.arek.petclinic.model.PetType;
 import pl.arek.petclinic.model.Vet;
 import pl.arek.petclinic.services.OwnerService;
 import pl.arek.petclinic.services.PetTypeService;
 import pl.arek.petclinic.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -38,11 +41,29 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("666 666 666");
+        Pet michaelsPet = new Pet();
+        michaelsPet.setName("Rosco");
+        michaelsPet.setOwner(owner1);
+        michaelsPet.setPetType(savedDogPetType);
+        michaelsPet.setBirthDate(LocalDate.now());
+        owner1.getPets().add(michaelsPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("666 666 666");
+        Pet fionasPet = new Pet();
+        fionasPet.setName("Puszek");
+        fionasPet.setOwner(owner2);
+        fionasPet.setPetType(saveCatPetType);
+        fionasPet.setBirthDate(LocalDate.now());
+        owner2.getPets().add(fionasPet);
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners...");
