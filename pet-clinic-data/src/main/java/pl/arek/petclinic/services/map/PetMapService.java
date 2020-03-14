9db1b1e02363598cea472407572c05fb41c5de74
Arkadiusz <arkadiusz.omyla@exaco.pl>
2020-03-14 13:42:24 +0100

    refactor names map services

--- pet-clinic-data/src/main/java/pl/arek/petclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/pl/arek/petclinic/services/map/PetMapService.java
@@ -1,31 +1,31 @@
 package pl.arek.petclinic.services.map;
 
 import org.springframework.stereotype.Service;
-import pl.arek.petclinic.model.PetType;
-import pl.arek.petclinic.services.PetTypeService;
+import pl.arek.petclinic.model.Pet;
+import pl.arek.petclinic.services.PetService;
 
 import java.util.Set;
 
 @Service
-public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
-    public Set<PetType> findAll() {
+    public Set<Pet> findAll() {
         return super.findAll();
     }
 
     @Override
-    public PetType findById(Long id) {
+    public Pet findById(Long id) {
         return super.findById(id);
     }
 
     @Override
-    public PetType save(PetType object) {
+    public Pet save(Pet object) {
         return super.save(object);
     }
 
     @Override
-    public void delete(PetType object) {
+    public void delete(Pet object) {
         super.delete(object);
     }
 
