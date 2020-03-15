086664f24d7c612f1faf947fbb119a0c667aa3c9
Arkadiusz <arkadiusz.omyla@exaco.pl>
2020-03-15 13:24:58 +0100

    added project lombok

--- pet-clinic-data/src/main/java/pl/arek/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/pl/arek/petclinic/model/Speciality.java
@@ -1,20 +1,20 @@
 package pl.arek.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "speciaities")
 public class Speciality extends BaseEntity {
     @Column(name = "description")
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
