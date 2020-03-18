2f3a94684d13de4a3f4c79626a07036f52fc8bde
Arkadiusz <arkadiusz.omyla@exaco.pl>
2020-03-18 20:33:13 +0100

    @ResponseBody

--- pet-clinic-web/src/main/java/pl/arek/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/arek/petclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package pl.arek.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.arek.petclinic.model.Vet;
 import pl.arek.petclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +23,10 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
